6db732bf5af97cb0d608521ee5c09f453c8e8f03
IgorShkidchenko <igorshkidchenko@gmail.com>
2018-12-28 14:38:32 +0200

    fix

--- lib/entities/generator.rb
+++ lib/entities/generator.rb
@@ -4,7 +4,7 @@ module LazyNavigator
     SUCCESS_BASH_MSG = 'bash script generated'.freeze
     SUCCESS_RUBY_MSG = 'ruby script generated'.freeze
     BASH_SCRIPT_NAME = 'last'.freeze
-    RUBY_SCRIPT_NAME = 'last_project'.freeze
+    RUBY_SCRIPT_NAME = 'last_project.rb'.freeze
 
     class << self
       def generate_script(folder = 'lazy_navigator')
@@ -26,7 +26,7 @@ module LazyNavigator
         Dir.mkdir(folder)
         Dir.chdir(folder) do
           File.new(RUBY_SCRIPT_NAME, 'w')
-          File.open(RUBY_SCRIPT_NAME, 'a').write("require 'LazyNavigator'\n\nPATH_TO_LAST_PROJECT = 'RubyGarage/codebreaker <-- change'")
+          File.open(RUBY_SCRIPT_NAME, 'a').write("require 'lazy_navigator'\n\nPATH_TO_LAST_PROJECT = 'RubyGarage/codebreaker <-- change me'")
           File.open(RUBY_SCRIPT_NAME, 'a').write("\n#IDE = your IDE bash name, for example VSC = code")
           File.open(RUBY_SCRIPT_NAME, 'a').write("\n\nLazyNavigator.lead(path: PATH_TO_LAST_PROJECT, ide: nil)")
         end
